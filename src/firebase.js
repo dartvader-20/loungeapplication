c3eee61cd6391a6b59ffa41fd70c17139415c3d6
dartvader-20 <chandanmohan50@gmail.com>
2023-10-22 09:17:06 +0530

    changes 2

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBheVRhUiNeOs_xRZcDNdIBiqtqYJyPmAQ",
-    authDomain: "lounge-application-ac21b.firebaseapp.com",
-    databaseURL: "https://lounge-application-ac21b-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "lounge-application-ac21b",
-    storageBucket: "lounge-application-ac21b.appspot.com",
-    messagingSenderId: "208829721166",
-    appId: "1:208829721166:web:34f0f91fc8e2a541be311f",
-    measurementId: "G-7FJ75TSW5T"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
